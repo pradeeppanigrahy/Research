60da6887470c7b897969e08e05095e1df4bed47d
pradeeppanigrahy <30314529+pradeeppanigrahy@users.noreply.github.com>
2017-12-19 18:24:28 +0530

    Power BI integration with Angular 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { ExperimentComponent } from './experiment/experiment.component';
+import {FormsModule} from '@angular/forms'
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import {AppRoutingModule} from './app.routes';
+import { AnalyticsComponent } from './analytics/analytics.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExperimentComponent,
+    HomeComponent,
+    HeaderComponent,
+    AnalyticsComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
